5c1599c3585aaaa1e97c15ccc0ee0d5b6ebabb19
Cxiao <cxiao@git.com>
2022-06-21 15:39:08 +0800

    发布版

---
+++ src/main/java/com/cheng/git/service/UserService.java
@@ -0,0 +1,17 @@
+package com.cheng.git.service;
+
+import org.junit.Test;
+
+/**
+ * @author Cxiao
+ * @date 2022/6/21 15:37
+ */
+public class UserService {
+
+    @Test
+    public void login() {
+        System.out.println("用户登录");
+    }
+
+
+}
