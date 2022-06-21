cdbb921410afc6715c21cf1126bcaaaeb5d1f526
Cxiao <cxiao@git.com>
2022-06-21 15:52:00 +0800

    优化性能

--- src/main/java/com/cheng/git/service/UserService.java
+++ src/main/java/com/cheng/git/service/UserService.java
@@ -8,14 +8,16 @@ import org.junit.Test;
  */
 public class UserService {
 
-    @Test
     public void login() {
         System.out.println("用户登录");
     }
 
-    @Test
     public void register() {
         System.out.println("用户注册");
+    }
+
+    public void service1() {
+        System.out.println("优化系统性能");
     }
 
 }
